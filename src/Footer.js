ec3b0ef888435564461913ab23f6b8c38964e701
mouad-eh <jm_elhaouari@esi.dz>
2022-05-20 23:48:08 +0100

    fixed some typos

--- src/Footer.js
+++ src/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
     <section id='footer'>
       <div className='footerFlex'>
         <h3>Mouad Elhaouari</h3>
-        <p>E-mail me at : jm_elhaouari@esi.dz</p>
+        <p>E-mail me at: jm_elhaouari@esi.dz</p>
       </div>
       <br></br>
       <p>Made with <BsFillSuitHeartFill color='red' /> by Me</p>
